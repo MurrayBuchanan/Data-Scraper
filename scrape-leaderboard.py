b87636d4752d70a5993049d47d5387d3805f87d7
Murray <40526053+MurrayBuchanan@users.noreply.github.com>
2023-06-17 18:12:23 +0100

    Added comments

--- scrape-leaderboard.py
+++ scrape-leaderboard.py
@@ -1,3 +1,9 @@
+
+# Issue: def scrape_leaderboard() has not been written to account for nested classes on websites. 
+# An example of this is on https://www.beatthestreet.me/leicester/
+
+
+
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta, date
@@ -7,14 +13,16 @@ import os.path
 
 # Scrape leaderboard data
 def scrape_leaderboard():
-    url = "https://mb-4.com" # EDIT THIS LINK TO CHANGE WHICH WEBSITE TO SCRAPE DATE FROM
+    url = "https://mb-4.com" # Link to website which is to be searches | This is my personal website which will be changed in the future
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    
     table = soup.find('table')
     rows = []
-    if table:
+    if table: 
         rows = [row for row in table.find_all('tr')]
-
+    
+    # Classes to be searches on the website | Edit this if tables, rows and columns are names something else
     leaderboard_data = []
     for row in rows[1:]:
         position = int(row.find(class_="RowItemstyled__StyledRowRank-sc-p1yhdf-2 igbMLU").text.strip())
@@ -26,26 +34,29 @@ def scrape_leaderboard():
     return leaderboard_data
 
 
-# Converts the number of points into distance (10 points == 500 meters)
+# Converts the number of points into distance | (10 points == 500 meters)
 def calculate_distance(points):
     return int((points / 10) * 5000)
 
 
-# Checks if the player participated
+# Checks if the player participated | (if player scores one or more points)
 def participation_check(points):
     return points > 0
 
 
 # Main method / Start
 def main():
-    game_duration = 4
+    # Gets current and end date
+    game_duration = 4 # Game length (in weeks)
     current_date = datetime.now().replace(hour=12, minute=0, second=0, microsecond=0)
     end_date = current_date + timedelta(weeks=game_duration)
-
+    
+    # Create a new dictionary to store leaderboard data
     leaderboard = {}
 
-    # Read existing data from the CSV file or initialize an empty leaderboard
+    # Read existing data from the .CSV file or creates one if it doesn't exist in the directory
     if os.path.isfile('leaderboard.csv'):
+        # Read file
         with open('leaderboard.csv', 'r', newline='') as file:
             reader = csv.reader(file)
             rows = list(reader)
@@ -69,7 +80,8 @@ def main():
                     'distance': calculate_distance(sum(points.values())),
                     'participation': participation_check(sum(points.values()))
                 }
-
+                
+    # Run program only if new day
     while current_date < end_date:
         leaderboard_data = scrape_leaderboard()
 
@@ -106,26 +118,29 @@ def main():
 
     # Updates leaderboard data in the CSV file
     try:
+        # Open file
         with open('leaderboard.csv', 'w', newline='') as file:
+            # Write to file
             writer = csv.writer(file)
             writer.writerow(['Total Players', 'Player ID', 'Points by Day', 'Cumulative Points', 'Total Distance (m)',
-                             'Participation'])
+                             'Participation']) # Table (column) headings
             total_players = len(leaderboard)
 
             for player_id, data in leaderboard.items():
+                # Run methods to calculate Cumulative Points, Total Distance, Participation Check
                 cumulative_points = sum(data['points'].values())
                 total_distance = calculate_distance(cumulative_points)
                 did_participate = participation_check(cumulative_points)
+                # Formats the points by day
                 points_by_day = [f"|{date.strftime('%Y-%m-%d')}:{points}| " for date, points in
                                  sorted(data['points'].items(), key=lambda x: x[0]) if points != 0]  # Filter out points with value 0
                 row = [total_players, player_id, ' '.join(points_by_day), cumulative_points, total_distance,
-                       did_participate]
+                       did_participate] # Table (column) content
                 writer.writerow(row)
-
-        print("Leaderboard data updated in leaderboard.csv\n")
+                
+        print("Leaderboard data updated in leaderboard.csv\n") # Print if working
     except Exception as e:
-        print("An error has occurred.\n", e)
-
+        print("An error has occurred.\n", e) # Print if error
 
 if __name__ == "__main__":
     main()
